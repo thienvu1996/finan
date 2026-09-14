6cb13536eee67bd6b6716f928712e3318460cd87
Thien Vu <101257780+thienvu1996@users.noreply.github.com>
2026-09-14 10:24:05 +0700

    Add personal finance center page

---
+++ app/money/page.tsx
@@ -0,0 +1,7 @@
+import PersonalFinanceCenter from "@/components/finance/personal-finance-center";
+
+export const dynamic = "force-dynamic";
+
+export default function MoneyPage() {
+  return <PersonalFinanceCenter />;
+}
